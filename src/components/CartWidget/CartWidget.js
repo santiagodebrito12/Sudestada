edde93012725b3eda0e9e60521e811ec12703c4c
santiagodebrito12 <santiago.debrito@live.com>
2021-07-06 12:17:32 -0300

    agregue animacion

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,17 +1,23 @@
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import {Link} from 'react-router-dom'
+import CartContext from '../../context/CartContext/CartContext';
 import carrito from '../../img/carrito.png';
 
 import './CartWidget.css';
 
 const CartWidget = () => {
- 
-    return (
+  const{Cart}=useContext(CartContext);
+  
+  
+
+  return (
       <>
-      <li><Link to="cart" className="enlances-nav"><img src={carrito}/><span className="items"></span></Link></li>
+      <li><Link to="cart" className="enlances-nav"><img src={carrito} alt="icono-carrito" className={Cart.length>0 ? "animate__animated animate__bounce animate__infinite" : "img-carrito"}/><span className="items"></span></Link></li>
       </>
         
       );
 }
  
-export default CartWidget
+export default CartWidget;
+
+
